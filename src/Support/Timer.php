82f67f95dde3b772ccce7d1a49410805acd097bc
Łukasz Witkowski <witul@linux.pl>
2020-07-19 14:32:55 +0200

    Update Timer.php

--- src/Support/Timer.php
+++ src/Support/Timer.php
@@ -1,20 +1,29 @@
 <?php
 
 namespace PragmaRX\Health\Support;
+use SebastianBergmann\Timer\Timer;
 
 class Timer
-{
+{    
+    protected $timer;
     public static function start()
     {
-        return class_exists('SebastianBergmann\Timer\Timer')
+        $this->timer = new Timer;
+        return $this->timer->start();
+/*        return class_exists('SebastianBergmann\Timer\Timer')
             ? \SebastianBergmann\Timer\Timer::start()
             : \PHP_Timer::start();
+*/            
     }
 
     public static function stop()
     {
+        $this->timer = new Timer;
+        return $this->timer->stop();
+        /*
         return class_exists('SebastianBergmann\Timer\Timer')
             ? \SebastianBergmann\Timer\Timer::stop()
             : \PHP_Timer::stop();
+        */
     }
 }
